e145afbe44802278fe96cdb3fe453db7fcb59a7a
Ben Coombes <benzcoombes@gmail.com>
2021-09-24 00:56:49 -0400

    reworked jump and double jump
    changed the way gravity gets applied to the player when falling makes the jump feel less floaty. Added a coroutine for double jump so it is on a timer and can't be spammed as well as a way to keep track of double jump charges.

--- COMP305 - Game Project X/Assets/Scripts/PlayerMovement.cs
+++ COMP305 - Game Project X/Assets/Scripts/PlayerMovement.cs
@@ -5,28 +5,33 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public enum PlayerState
-    {        
+    {
         isWalking,
         isFalling,
         isGrounded,
         isStaggering
-    }    
+    }
 
-    [SerializeField] private float speed, jumpForce; // made it private but still accessible through inspecter 
+    [SerializeField] private float speed, jumpVelocity; // made it private but still accessible through inspecter 
     [SerializeField] public PlayerState currentState;
 
-    private float timer = 0, jumpTime = 0.4f;    
-    private Rigidbody2D rb;      
+    private float jumpCooldown = 0.4f, fallMultiplier = 2f, lowJumpMultiplier = 3.5f;
+    private Rigidbody2D rb;
+    public int jumpCharges, totalJumps = 5;
+    public bool jumpReady;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         currentState = PlayerState.isGrounded;
+        jumpCharges = totalJumps;
+        jumpReady = true;
+        StartCoroutine(DoubleJump(jumpCooldown));
     }
 
     void Update()
     {
-        Movement();        
+        Movement();
     }
 
     private void ChangeState(PlayerState newState)
@@ -40,33 +45,53 @@ public class PlayerMovement : MonoBehaviour
     private void Movement()
     {
         float horiz = Input.GetAxisRaw("Horizontal");
-        float vert = rb.velocity.y;
-
         transform.position += new Vector3(horiz, 0, 0) * Time.deltaTime * speed;
 
-        if (currentState == PlayerState.isGrounded && Input.GetButtonDown("Jump") && timer > jumpTime)
+        //Changes gravity so you fall faster (makes it feel less floaty when jumping)
+        if (rb.velocity.y < 0)
         {
-            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
-            ChangeState(PlayerState.isFalling);
-            timer = 0;
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        //if jump is not held more gravity is applied causing a shorter jump (tap jump for less height, hold for more height)
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
         }
 
-        if (currentState != PlayerState.isGrounded && Input.GetButtonDown("Jump"))
+        if (currentState == PlayerState.isGrounded && Input.GetButtonDown("Jump"))
         {
-            rb.velocity = new Vector2(0.0f, 0.0f);
-            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);            
-            Debug.Log("vert");
-            timer = 0;
+            rb.velocity = Vector2.up * jumpVelocity;
+            ChangeState(PlayerState.isFalling);
+
         }
-        
-        timer += Time.deltaTime;
+        else if (currentState != PlayerState.isGrounded && Input.GetButtonDown("Jump") && jumpReady == true && jumpCharges > 0)
+        {
+            jumpReady = false;
+            rb.velocity = new Vector2(0.0f, 0.0f);
+            rb.velocity = Vector2.up * jumpVelocity;
+        }
+    }
+    private IEnumerator DoubleJump(float jumpCooldown)
+    {
+        while (true)
+        {
+            if (jumpReady == false)
+            {
+                yield return new WaitForSecondsRealtime(jumpCooldown);
+                jumpCharges -= 1;
+                jumpReady = true;
+            }
+            yield return null;
+        }
+
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Platform"))
         {
+            jumpCharges = totalJumps;
             ChangeState(PlayerState.isGrounded);
-        }        
+        }
     }
 }
