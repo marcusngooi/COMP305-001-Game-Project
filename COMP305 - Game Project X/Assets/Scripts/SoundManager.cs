a81606b1128780e60f5669a5097f903ac6a17c6d
grant <gmacmil3@my.centennialcollege.ca>
2021-11-03 15:07:18 -0400

    Made functioning Volume Slider
    volume now works for in game music

---
+++ COMP305 - Game Project X/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+
+    public void ChangeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+    }
+}
