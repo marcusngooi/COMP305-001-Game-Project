338b4a2dd626f9303934c48aad8726afe06631b2
Marcus Ngooi <mngooi@gmail.com>
2021-11-06 17:13:39 -0400

    Added quit function in Unity; organized game objects

--- COMP305 - Game Project X/Assets/Scripts/MainMenu.cs
+++ COMP305 - Game Project X/Assets/Scripts/MainMenu.cs
@@ -12,7 +12,10 @@ public class MainMenu : MonoBehaviour
 
     public void QuitGame()
     {
-        Debug.Log("Quitting Game...");
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
         Application.Quit();
+#endif
     }
 }
