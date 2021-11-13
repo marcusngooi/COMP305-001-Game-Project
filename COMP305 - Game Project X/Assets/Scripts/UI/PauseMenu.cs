d3a9f0f7205c1e78c676f5cdf48cfcb55a375403
Ikamjot Hundal <ihundal@my.centennialcollege.ca>
2021-11-13 16:49:55 -0500

    Made Pause Menu functional in its basic form

---
+++ COMP305 - Game Project X/Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenu;
+    private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+
+            Pause();
+              
+            }
+        }
+    public void Pause()
+    {        
+       pauseMenu.SetActive(true);
+       Time.timeScale = 0f;
+        
+    }
+
+    
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void Quit()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Game_Over");
+    }
+
+}
