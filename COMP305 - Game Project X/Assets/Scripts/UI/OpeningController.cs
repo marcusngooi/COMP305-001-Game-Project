fd1eedc5663065e861af2e7db9fb10e4df354c4e
Ben Coombes <benzcoombes@gmail.com>
2021-11-24 12:23:55 -0500

    changes to opening scene

--- COMP305 - Game Project X/Assets/Scripts/UI/OpeningController.cs
+++ COMP305 - Game Project X/Assets/Scripts/UI/OpeningController.cs
@@ -7,7 +7,10 @@ using UnityEngine.UI;
 public class OpeningController : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI txt;
+    [SerializeField] TextMeshProUGUI continueText;
     string story;
+    bool textDone;
+    [SerializeField] SceneTransition sceneTransition;
 
     void Awake()
     {
@@ -21,10 +24,20 @@ public class OpeningController : MonoBehaviour
 
     IEnumerator PlayText()
     {
+        yield return new WaitForSeconds(2f);
         foreach (char c in story)
         {
             txt.text += c;
             yield return new WaitForSeconds(0.02f);
+        }
+        textDone = true;
+        continueText.gameObject.SetActive(true);
+    }
+    private void Update()
+    {
+        if(textDone && Input.anyKey)
+        {
+            sceneTransition.PlayLevel(2);
         }
     }
 }
