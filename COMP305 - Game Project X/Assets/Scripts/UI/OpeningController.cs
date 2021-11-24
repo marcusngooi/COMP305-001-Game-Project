ab86ca2a0928299bdf2c9bd2b7ef21f70904ae81
Ben Coombes <benzcoombes@gmail.com>
2021-11-24 13:17:11 -0500

    sound for opening scene

--- COMP305 - Game Project X/Assets/Scripts/UI/OpeningController.cs
+++ COMP305 - Game Project X/Assets/Scripts/UI/OpeningController.cs
@@ -9,8 +9,10 @@ public class OpeningController : MonoBehaviour
     [SerializeField] TextMeshProUGUI txt;
     [SerializeField] TextMeshProUGUI continueText;
     string story;
-    bool textDone;
+    bool textDone, skipText;
     [SerializeField] SceneTransition sceneTransition;
+    public AudioSource audioSource;
+    public AudioClip typeAudio;
 
     void Awake()
     {
@@ -19,25 +21,46 @@ public class OpeningController : MonoBehaviour
         txt.text = "";
 
         // TODO: add optional delay when to start
-        StartCoroutine("PlayText");
+        StartCoroutine(PlayText());
+        StartCoroutine(PlayTypeSound());
     }
 
     IEnumerator PlayText()
     {
         yield return new WaitForSeconds(2f);
+        audioSource.Play();
         foreach (char c in story)
         {
             txt.text += c;
             yield return new WaitForSeconds(0.02f);
+            if (skipText == true)
+            {
+                txt.text = story;
+                break;
+            }
         }
+        audioSource.Stop();
         textDone = true;
         continueText.gameObject.SetActive(true);
     }
+    IEnumerator PlayTypeSound()
+    {
+        while(textDone == false)
+        {
+            
+            yield return new WaitForSeconds(0.12f);
+        }
+        yield return null;
+    }
     private void Update()
     {
-        if(textDone && Input.anyKey)
+        if(textDone && Input.anyKeyDown)
         {
             sceneTransition.PlayLevel(2);
+        }
+        if(textDone == false && Input.anyKeyDown && skipText == false)
+        {
+            skipText = true;
         }
     }
 }
