75af7a1be50d56f83220c75799ad112e63049be9
Ben Coombes <benzcoombes@gmail.com>
2021-11-25 16:53:25 -0500

    resolution, vsync and fullscreen settings added(will only work in build)

--- COMP305 - Game Project X/Assets/Scripts/Menu/SettingsMenuController.cs
+++ COMP305 - Game Project X/Assets/Scripts/Menu/SettingsMenuController.cs
@@ -2,11 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro;
 
 public class SettingsMenuController : MonoBehaviour
 {
     public GameObject mainMenu;
     public EventSystem eventSystem;
+    public Toggle fullscreenTog, vsyncTog;
+    public List<ResItem> resolutions = new List<ResItem>();
+    private int selectedResolution;
+    public TMP_Text resolutionLabel;
+
+    public void Start()
+    {
+        fullscreenTog.isOn = Screen.fullScreen;
+        if(QualitySettings.vSyncCount == 0)
+        {
+            vsyncTog.isOn = false;
+        }else
+        {
+            vsyncTog.isOn = true;
+        }
+        bool foundRes = false;
+
+        for(int i = 0; i < resolutions.Count; i++)
+        {
+            if(Screen.width == resolutions[i].horizontal && Screen.height == resolutions[i].vertical)
+            {
+                foundRes = true;
+                selectedResolution = i;
+                UpdateResLabel();
+            }
+        }
+    }
     public void SettingsMenu()
     {
         this.gameObject.SetActive(true);
@@ -25,4 +54,46 @@ public class SettingsMenuController : MonoBehaviour
         this.gameObject.SetActive(false);
         mainMenu.SetActive(true);
     }
+
+    public void ApplyGraphics()
+    {
+        //Screen.fullScreen = fullscreenTog.isOn;
+
+        if (vsyncTog.isOn)
+        {
+            QualitySettings.vSyncCount = 1;
+        } else
+        {
+            QualitySettings.vSyncCount = 0;
+        }
+
+        Screen.SetResolution(resolutions[selectedResolution].horizontal, resolutions[selectedResolution].vertical, fullscreenTog.isOn);
+    }
+    public void ResLeft()
+    {
+        selectedResolution--;
+        if(selectedResolution < 0)
+        {
+            selectedResolution = 0;
+        }
+        UpdateResLabel();
+    }
+    public void ResRight()
+    {
+        selectedResolution++;
+        if (selectedResolution > resolutions.Count - 1)
+        {
+            selectedResolution = resolutions.Count - 1;
+        }
+        UpdateResLabel();
+    }
+    public void UpdateResLabel()
+    {
+        resolutionLabel.text = resolutions[selectedResolution].horizontal.ToString() + " x " + resolutions[selectedResolution].vertical.ToString();
+    }
+}
+[System.Serializable]
+public class ResItem
+{
+    public int horizontal, vertical;
 }
