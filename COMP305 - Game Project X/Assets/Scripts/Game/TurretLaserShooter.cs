591a9b93a46a63790ab431aaf1823ddf5ddcc78a
Sukhmannat Singh <thegamingtitan17@gmail.com>
2021-12-09 14:12:39 -0500

    Turret Changes

--- COMP305 - Game Project X/Assets/Scripts/Game/TurretLaserShooter.cs
+++ COMP305 - Game Project X/Assets/Scripts/Game/TurretLaserShooter.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class TurretLaserShooter : MonoBehaviour
 {
     [SerializeField] private GameObject laser, laserSpawn;
-    [SerializeField] private float fireRate;
+    [SerializeField] [Range(0.01f, 1.99f)] private float fireRate;
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +21,7 @@ public class TurretLaserShooter : MonoBehaviour
             gObj = GameObject.Instantiate(laser, laserSpawn.transform.position, laserSpawn.transform.rotation);
             gObj.transform.Rotate(new Vector3(0, 0, 90));
 
-            yield return new WaitForSeconds(fireRate);
+            yield return new WaitForSeconds(2 - fireRate);
         }
     }
 }
